7ebfd78a2cd3b6f269c12a930754a812353ae9c1
Mariano Feresin <77126767+marianoferesin@users.noreply.github.com>
2021-05-26 16:57:24 -0300

    Update Plantilla Competitva.cpp

--- Plantilla Competitva.cpp
+++ Plantilla Competitva.cpp
@@ -2,17 +2,7 @@
 #define forn(i,n) for(int i = 0; i<n; i++)
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int main() {
 	
-	int a[2] = {1,2};
-	
-	auto [x,y] = a; // creates e[2], copies a into e, then x refers to e[0], y refers to e[1]
-	auto& [xr, yr] = a; // xr refers to a[0], yr refers to a[1]
-	
-	
-	cout<<x<<y<<endl;
 	return 0;
 }
-
-
-
