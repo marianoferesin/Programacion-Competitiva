9a1c00cd52481763aef4444a9149f3434be53cc5
Mariano Feresin <77126767+marianoferesin@users.noreply.github.com>
2021-05-26 16:22:32 -0300

    plantilla básica para programación competitiva

---
+++ Plantilla Competitva.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define forn(i,n) for(int i = 0; i<n; i++)
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	
+	int a[2] = {1,2};
+	
+	auto [x,y] = a; // creates e[2], copies a into e, then x refers to e[0], y refers to e[1]
+	auto& [xr, yr] = a; // xr refers to a[0], yr refers to a[1]
+	
+	
+	cout<<x<<y<<endl;
+	return 0;
+}
+
+
+
