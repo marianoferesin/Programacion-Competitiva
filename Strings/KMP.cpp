b3f154ba8a7968acdd14d29b38fc90a048a93aa0
Lascot29 <78239004+Lascot29@users.noreply.github.com>
2021-06-03 14:46:49 -0300

    Create KMP.cpp

---
+++ Strings/KMP.cpp
@@ -0,0 +1,33 @@
+void kmp(const string &s,const string &t){
+	int n = s.size(),m = t.size();
+	vector<int> v(m);
+	//Preprocesamiento
+	int i=1,j=0;
+	while(i<m){
+		if(t[i]==t[j]){
+			v[i] = j+1;
+			i++; j++;
+		}
+		else{
+			if(j)
+				j = v[j-1];
+			else{
+				v[i] = 0;
+				i++;
+			}
+		}
+	}
+	//KMP
+	i = 0; j = 0;
+	while(i<n){
+		if(s[i]==t[j])
+			j++;
+		else
+			j = j ? v[j-1] : 0;
+		i++;
+		if(j==m){
+			cout<<"ans: "<<i-m+1<<endl;//pos del patron
+			j = 0;
+		}
+	}
+}
