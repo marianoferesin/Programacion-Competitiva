894be4e41915b15b43d38191687e479b29be83a4
Mariano Feresin <77126767+marianoferesin@users.noreply.github.com>
2021-07-25 18:59:00 -0300

    Update Toposort.cpp

--- Grafos/Toposort.cpp
+++ Grafos/Toposort.cpp
@@ -1,23 +1,30 @@
+//topological sort -- detecta ciclos :D
 int n; // number of vertices
-vector<vector<int>> adj; // adjacency list of graph
-vector<bool> visited;
+vector<set<int>> adj; // adjacency list of graph
+vector<int> color; //visited->0 - on stack->1 - ready->2
 vector<int> ans;
 
-void dfs(int v) {
-    visited[v] = true;
+bool dfs(int v) {
+    bool acyclic = true;
+    color[v] = 1;
     for (int u : adj[v]) {
-        if (!visited[u])
-            dfs(u);
+	if (color[u]==1) return false; // has a cycle
+        if (color[u]==0)
+            acyclic = min(dfs(u), acyclic);
     }
+    color[v] = 2;
     ans.push_back(v);
+    return acyclic;
 }
 
-void topological_sort() {
-    visited.assign(n, false);
+bool topological_sort() { //false if there is a cylcle otherwise toposort is in ans
+    color.assign(n, 0);
     ans.clear();
+    bool acyclic = true;
     for (int i = 0; i < n; ++i) {
-        if (!visited[i])
-            dfs(i);
+        if (color[i]==0)
+            acyclic = min(dfs(i),acyclic);
     }
     reverse(ans.begin(), ans.end());
+    return acyclic;
 }
