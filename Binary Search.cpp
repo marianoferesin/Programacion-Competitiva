97fcd1f09d0a044e665fdf8c0196f8caa622d41a
Mariano Feresin <77126767+marianoferesin@users.noreply.github.com>
2021-07-25 21:10:42 -0300

    Create Binary Search.cpp

---
+++ Binary Search.cpp
@@ -0,0 +1,13 @@
+//en l queda guardado el lower bound;
+	vector <int> v = {-8,-2,0,0,0,3,3,3,4,5,6,7};
+	int n = v.size();
+	int x = 4;
+	int l=0, r=n-1;
+	while(l<r){
+	    int mid = l+(r-l)/2;
+    
+	    if(v[mid] >= x) r = mid;
+	    else l = mid+1;
+	}
+	    
+	cout<<"x: "<<x<<" -> "<<v[l]<<endl;
