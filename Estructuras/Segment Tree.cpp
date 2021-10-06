7a2073a8e83072bf0438b536d5419c0e582c4fea
unknown <marianoferesin@gmail.com>
2021-10-05 21:31:13 -0300

    se agrego segment tree

---
+++ Estructuras/Segment Tree.cpp
@@ -0,0 +1,54 @@
+typedef long long tint;
+const int mxn = 1<<20; //cambiarlo
+struct Data{
+	tint s;
+	Data(tint _s=0){
+		s = _s;
+	}
+	void merge(const Data& l,const Data& r){
+		s = l.s+r.s;
+	}
+};
+struct segTree{
+	Data data[2*mxn];
+	int n;
+	Data* init(const int m){
+		n = 1<<(32-__builtin_clz(m));
+		for(int i=0;i<2*n;i++){
+			data[i] = Data();
+		}
+		return (data+n);
+	}
+	void build(){
+		for(int i=n-1;i>0;i--){
+			data[i].merge(data[2*i],data[2*i+1]);
+		}
+	}
+	void update(int pos,const Data& w){update(pos,w,0,n-1,1);}
+	void update(int pos,const Data& w,int l,int r,int k){
+		if(l==r){
+			data[k] = w;
+			//data[k].merge(data[k],w);
+		}
+		else{
+			int medio = (l+r)/2;
+			if(pos<=medio)
+				update(pos,w,l,medio,2*k);
+			else
+				update(pos,w,medio+1,r,2*k+1);
+			data[k].merge(data[2*k],data[2*k+1]);
+		}
+	}
+	Data query(int a,int b){return query(a,b,1,0,n-1);}
+	Data query(int a,int b,int k,int l,int r){
+		//[l,r] es el rango actual, [a,b] es la query, k posicion del vector
+		if(r<a or l>b)
+			return Data(); //poner "neutro" ej 1e18 para min queries
+		if(a<=l and r<=b)
+			return data[k];
+		int medio = (l+r)/2;
+		Data ret = Data();
+		ret.merge(query(a,b,2*k,l,medio),query(a,b,2*k+1,medio+1,r));
+		return ret;
+	}
+};
